8b94075ceeff1d747685d00dd0b1bea929a893b3
Adrian Zech <adrian@zech.co>
2025-03-14 11:30:47 +0100

    Added email validation to registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +20,13 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'app_email',
+                'attr'  => [
+                    'placeholder' => 'app_email_placeholder',
+                    'autofocus'   => true,
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped'      => false,
                 'constraints' => [
